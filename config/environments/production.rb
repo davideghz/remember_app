a9b9aa6f0c5c61ef8c7f55c0f56e1e1715906e2b
Davide Ghezzi <davideghz@gmail.com>
2016-04-15 16:14:20 +0200

    add asset_host to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.asset_host = 'https://damp-wave-59763.herokuapp.com'
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'damp-wave-59763.herokuapp.com'
